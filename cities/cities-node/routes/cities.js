94a6cd45d36299439be9af816729956e18ee6430
Rajesh Jain <rajesh.jain@pivotal.io>
2015-06-11 17:07:48 -0400

    Added a simple Node App

--- cities/cities-node/routes/cities.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET citiespage. */
-router.get('/', function(req, res) {
-	res.render('cities', { title: 'Spring Cloud App' });
-});
-
-module.exports = router;
